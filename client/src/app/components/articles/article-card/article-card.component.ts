ddff543eb66a3c871f223a849864135cb08bc508
pawelsmolinski <pawel.smolinski94@gmail.com>
2022-11-20 08:08:35 +0100

    fix wcag card contrast

--- client/src/app/components/articles/article-card/article-card.component.ts
+++ client/src/app/components/articles/article-card/article-card.component.ts
@@ -3,6 +3,7 @@ import { Article } from 'src/app/model/Article';
 import { GenerateService } from 'src/app/services/generate.service';
 
 import { Component, Input, OnInit, Output } from '@angular/core';
+import { ContrastService } from 'src/app/services/contrast.service';
 
 @Component({
   selector: 'app-article-card',
@@ -13,21 +14,35 @@ export class ArticleCardComponent extends BaseComponent implements OnInit {
   @Input() article!: Article;
 
   isLoading: boolean = false;
+  highContrastColorClass: string = ''
 
-  constructor(private readonly generateService: GenerateService) {
+  constructor(
+    private readonly generateService: GenerateService, 
+    private readonly contrastService: ContrastService
+  ) {
     super();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subs.add(this.contrastService.contrast$.subscribe((isHighContrast) => {
+          console.log('isHighContrast', isHighContrast)
+          if (isHighContrast) {
+            this.highContrastColorClass = 'hover:bg-yellow-400 hover:text-black';
+          } else {
+            this.highContrastColorClass = '';
+          }
+        }
+      ));
+  }
 
   generate() {
     this.isLoading = true
 
-    this.subs.sink = this.generateService
+    this.subs.add(this.generateService
       .generate(this.article.url, this.article.title)
       .subscribe(() => {
         this.isLoading = false;
         console.log('Generated');
-      });
+      }));
   }
 }
