4779da30b5de374cca48eb68869bb9de054445c9
steciuk <a.t.steciuk@gmail.com>
2022-11-20 10:57:05 +0100

    gh pages

--- client/src/app/model/Article.ts
+++
@@ -1,5 +0,0 @@
-export type Article = {
-  url: string;
-  title: string;
-  description: string;
-};
--- client/src/app/model/GenerateRequestDTO.ts
+++
@@ -1,7 +0,0 @@
-export class GenerateRequestDTO {
-  constructor(
-    public readonly sourceUrl: string,
-    public readonly title: string,
-    public readonly num_pages: number
-  ) {}
-}
